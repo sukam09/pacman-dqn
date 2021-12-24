ed079d05ec8582bafcedddd1a09cf3ec5502e381
sukam09 <sukam09@naver.com>
2021-12-24 13:25:44 +0900

    Complete original dqn test

--- pacmanDQNAgents.py
+++ pacmanDQNAgents.py
@@ -225,12 +225,12 @@ class PacmanDQN(PacmanUtils):
         pacman_pos = state.getPacmanPosition()
         ghost_pos = state.getGhostPosition(1)  # Max: 1
         food_num = state.getNumFood()
+       
         if len(state.getCapsules()) == 1:
             capsule_pos = state.getCapsules()[0]
         else:
             capsule_pos = (-1, -1)
-        score = state.getScore()
-        scared_timer = state.getScaredTimer()
+
         food = state.getFood()
         food_pos = []
 
@@ -241,8 +241,6 @@ class PacmanDQN(PacmanUtils):
                     food_pos.append([i, j])
         
         # Feature vector
-        # res = [pacman_pos[0], pacman_pos[1], ghost_pos[0], ghost_pos[1], food_num,
-        #         capsule_pos[0], capsule_pos[1], scared_timer]
         res = [pacman_pos[0], pacman_pos[1], ghost_pos[0], ghost_pos[1], food_num,
                 capsule_pos[0], capsule_pos[1]]
 
@@ -253,14 +251,6 @@ class PacmanDQN(PacmanUtils):
             res.extend([food_pos[0][0], food_pos[0][1], -1, -1])
         else:
             res.extend([-1, -1, -1, -1])
-        
-        # dist_to_food_1 = np.sqrt((food_pos[0][0] - pacman_pos[0]) ** 2 + (food_pos[0][1] - pacman_pos[1]) ** 2)
-        # dist_to_food_2 = np.sqrt((food_pos[1][0] - pacman_pos[0]) ** 2 + (food_pos[1][1] - pacman_pos[1]) ** 2)
-        # res.extend([dist_to_food_1, dist_to_food_2])
-
-        # res = minmax_scale(res)
-        # self.input_size = len(res)
-        # print(res)  # For debug
 
         return np.array(res)
 
